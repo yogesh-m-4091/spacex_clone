6d2299c0ae18cb219dc95fe30c6821d9a321c3f5
yogesh-m-4091 <yogimagadumr@gmail.com>
2023-04-22 18:33:23 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Spacex from "./SpaceX/Spacex";
+const App=()=>
+{
+    return(
+        <div>
+            
+           
+            <Spacex></Spacex>
+     
+
+        </div>
+    )
+}
+export default App;
